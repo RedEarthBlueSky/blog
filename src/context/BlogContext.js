723e616d2336eb9f34c29ecc465bb5aa0e2ad5a3
redearthbluesky <ian@isalt.digital>
2024-08-01 12:46:09 +0100

    refactor to automate context creation

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,29 +1,20 @@
-import React, { createContext, useState } from 'react'
-// import { blogPosts } from '../data'
+import createDataContext from './createDataContext'
 
-const BlogContext = createContext()
-
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([])
-
-  const addBlogPost = () => {
-    setBlogPosts([
-      ...blogPosts, 
-      { 
-        title: `Blog Post #${blogPosts.length + 1}`, 
-        body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      }
-    ])
+const blogReducer = (state, action) => {
+  switch(action.type) {
+    case 'add_blogpost':
+      return [...state, {title: `Blog Post #${state.length + 1}`}]
+    default:
+      return state
   }
-
-  return (
-    <BlogContext.Provider 
-    //  data is the state variable, addBlogPost the updater
-      value={{data: blogPosts, addBlogPost, setBlogPosts}}
-    >
-      {children}
-    </BlogContext.Provider>
-  )
 }
 
-export default BlogContext
+const addBlogPost = () => {
+  dispatch({ type: 'add_blogpost'})
+}
+
+export const { Context, Provider } = createDataContext(
+    blogReducer, 
+    { addBlogPost },
+    []
+  )
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,42 +1,15 @@
 import { View, Text, FlatList, Button } from 'react-native'
-import React, { useContext } from 'react'
+import React from 'react'
 import styles from '../styles'
-import BlogContext from '../context/BlogContext'
-
 
 const IndexScreen = () => {
-  //  The job of the Blog Post Provider is to hand down an object 
-  //  that has our current blogs and a way to change it in a 
-  //  value={{state, update}} format, here is is 
-  //  value={{data: blogPosts, addBlogPost}}
-  const { data, addBlogPost, setBlogPosts } = useContext(BlogContext)
+
   return (
     <View>
       <Text style={styles.H1}>Index Screen</Text>
       <Button 
         title='Add Post'
-        onPress={addBlogPost}
-      />
-      <View style={{marginTop: 10}}></View>
-      <Button 
-        title='Reset Posts'
-        onPress={() => setBlogPosts([])}
-      />
-      <FlatList 
-        data={data}
-        keyExtractor={(post) => post.title}
-        renderItem={({ item }) => {
-          return (
-            <View>
-              <Text style={styles.H5}>
-                {item.title}
-              </Text>
-              <Text style={styles.H6}>
-                {item.body}
-              </Text>
-            </View>
-          )
-        }}
+        onPress={() => null}
       />
     </View>
   )
