d342b9dbc8926aeaa89991a230b727e38833392d
redearthbluesky <ian@isalt.digital>
2025-03-18 21:26:49 +0000

    Update context state management to useReducer

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,28 @@
-import React, { useState } from "react"
+import React, { useReducer } from "react"
 const BlogContext = React.createContext()
 
-//  named export requires {} braces for import
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([])
-
-  const addBlogPost = () => {
-    setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}])
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_blogpost' :
+      return [...state, { title: `Blog Post #${state.length + 1}`}]
+    case 'clear_blogposts' :
+      return []
+    
+    default :
+      return state
   }
+}
+
+export const BlogProvider = ({ children }) => {
+  const [blogPosts, dispatch ] = useReducer(blogReducer, [])
+
+  const addBlogPost = () => { dispatch({ type: 'add_blogpost'}) }
+  const clearBlogPosts = () => { dispatch({ type: 'clear_blogposts'})}
+  const removeBlogPost = () => { dispatch({ type: 'remove_blogpost'})}
+  const editBlogPost = () => { dispatch({ type: 'edit_blogpost'})}
 
   return (
-    <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
+    <BlogContext.Provider value={{data: blogPosts, addBlogPost, clearBlogPosts }}>
       {children}
     </BlogContext.Provider>
   )
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,22 +4,30 @@ import BlogContext from '../context/BlogContext'
 import { parentStyles } from '../styles/styles'
 
 const IndexScreen = () => {
-  const blogPosts = useContext(BlogContext)
-  // console.log(blogPosts)
+  const { data, addBlogPost, clearBlogPosts } = useContext(BlogContext)
+
   return (
     <View style={{padding: 10}}>
       <Text style={parentStyles.h2bold}>Index Screen</Text>
       <TouchableOpacity 
         style={parentStyles.TOButton}
-        onPress={() => blogPosts.addBlogPost()}
+        onPress={addBlogPost}
       >
         <Text style={parentStyles.TOButtonText}>Add Blog Post</Text>
       </TouchableOpacity>
+
+      <TouchableOpacity 
+        style={parentStyles.TOButton}
+        onPress={clearBlogPosts}
+      >
+        <Text style={parentStyles.TOButtonText}>Clear Blog Posts</Text>
+      </TouchableOpacity>
+      
       <FlatList 
-        data={blogPosts.data}
+        data={data}
         keyExtractor={(blogPost) => blogPost.title}
-        renderItem={({ item }) => {
-          return <Text style={parentStyles.h3bold}>{item.title}</Text>
+        renderItem={({ item, index }) => {
+          return <Text style={parentStyles.h3bold}>{index + 1}. {item.title}</Text>
         }}
       />
     </View>
